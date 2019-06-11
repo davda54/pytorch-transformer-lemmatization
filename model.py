6fc2177771ee0f10b9332bea5c0d32ab092106b4
David Samuel <david.samuel@seznam.cz>
2019-06-11 19:57:01 +0200

    type and shape hint with tsalib

--- model.py
+++ model.py
@@ -4,26 +4,49 @@ import torch
 import torch.nn as nn
 from morpho_dataset import MorphoDataset
 
+from tsalib import dim_vars
+B, T, D, H, E = dim_vars('Batch SeqLength Dim NumHeads PositionalEmbedding')
+T_K, T_Q = dim_vars('SeqLengthKey SeqLengthQuery')
+S, W, C = dim_vars('Sentences Words Chars')
 
-def arrange_char_pos_embedding(len_k, len_q, max_len, embedding):
-    k = torch.arange(len_k, device='cuda')
-    q = torch.arange(len_q, device='cuda')
-    indices = k.view(1, -1) - q.view(-1, 1)
+
+def arrange_char_pos_embedding(len_k: int, len_q: int, max_len: int, embedding: (E,D//H)) -> (T_Q,T_K,D//H):
+    k: (T_K) = torch.arange(len_k, device='cuda')
+    q: (T_Q) = torch.arange(len_q, device='cuda')
+    indices: (T_Q,T_K) = k.view(1, -1) - q.view(-1, 1)
     indices.clamp_(-max_len, max_len).add_(max_len)
 
     return embedding[indices, :]
 
-def arrange_word_pos_embedding(len_k, lens_q, max_len, embedding):
-    k = torch.arange(len_k, device='cuda')
-    q = torch.repeat_interleave(lens_q - lens_q.cumsum(dim=0), lens_q, dim=0) + torch.arange(lens_q.sum(), device='cuda')
-    indices = k.view(1, -1) - q.view(-1, 1)
+def arrange_word_pos_embedding(len_k: int, lens_q: int, max_len: int, embedding: (E,D//H)) -> (B,T_K,D//H):
+    k: (T_K) = torch.arange(len_k, device='cuda')
+    q: (B) = torch.repeat_interleave(lens_q - lens_q.cumsum(dim=0), lens_q, dim=0) + torch.arange(lens_q.sum(), device='cuda')
+    indices: (B,T_K) = k.view(1, -1) - q.view(-1, 1)
     indices.clamp_(-max_len, max_len).add_(max_len)
 
     return embedding[indices, :]
 
-class UniformAttentionSublayer(nn.Module):
+
+class PositionLinear(torch.nn.Module):
+    def __init__(self, in_dim: int, out_dim: int) -> None:
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        w = torch.empty(in_dim, out_dim)
+        torch.nn.init.normal_(w, std=0.015)
+        self.w: (in_dim, out_dim) = nn.Parameter(w)
+        self.b: (out_dim,) = nn.Parameter(torch.zeros(out_dim))
+
+    def forward(self, x) -> torch.Tensor:
+        size_out = (self.out_dim,) + x.size()[:-1]
+        x = torch.addmm(self.b, x.view(-1, x.size(-1)), self.w)
+        return x.view(*size_out)
+
+
+class AttentionSublayer(nn.Module):
     def __init__(self, dimension, heads, max_pos_len, attention_dropout):
-        super(UniformAttentionSublayer, self).__init__()
+        super(AttentionSublayer, self).__init__()
 
         self.dimension = dimension
         self.heads = heads
@@ -31,107 +54,44 @@ class UniformAttentionSublayer(nn.Module):
         self.scale = math.sqrt(dimension / heads)
 
         self.pos_embedding = torch.randn(2*max_pos_len + 1, dimension // heads, device='cuda', requires_grad=True)
-        self.input_transform = nn.Linear(dimension, 3*dimension, bias=False)
-        self.softmax = nn.Softmax(dim=3)
+        self.input_transform_q = PositionLinear(dimension, dimension)
+        self.input_transform_k = PositionLinear(dimension, dimension)
+        self.input_transform_v = PositionLinear(dimension, dimension)
         self.attention_dropout = nn.Dropout(attention_dropout)
-        self.output_transform = nn.Linear(dimension, dimension, bias=False)
+        self.output_transform = PositionLinear(dimension, dimension)
 
-        #self.init_weights()
-
-    def init_weights(self):
-        nn.init.normal_(self.input_transform.weight, mean=0, std=np.sqrt(2.0 / (self.dimension + self.dimension // self.heads)))
-        nn.init.zeros_(self.input_transform.bias)
-        nn.init.xavier_normal_(self.output_transform.weight)
-        nn.init.zeros_(self.output_transform.bias)
-
-    def forward(self, input, mask):
-        batch_size, seq_len, _ = input.size()
-
-        QKV = self.input_transform(input)
-        QKV = QKV.view(batch_size, -1, self.heads, 3*self.dimension // self.heads)
-        QKV = QKV.permute(0, 2, 1, 3).contiguous()
-        Q, K, V = QKV.chunk(3, dim=3)
-
-        logits = torch.matmul(Q, K.transpose(2, 3))
-
-        arranged_pos = arrange_char_pos_embedding(seq_len, seq_len, self.max_pos_len, self.pos_embedding)
-        Q_t = Q.permute(2, 0, 1, 3).view(seq_len, batch_size*self.heads, -1)
-        pos_logits = torch.matmul(Q_t, arranged_pos.transpose(1, 2))
-        pos_logits = pos_logits.view(seq_len, batch_size, self.heads, -1).permute(1, 2, 0, 3)
-        logits = (logits + pos_logits) / self.scale
-
-        if mask is not None: logits.masked_fill_(mask, -np.inf)
-        indices = self.softmax(logits)
-        indices = self.attention_dropout(indices)
-        combined = torch.matmul(indices, V)
-
-        heads_concat = combined.permute(0, 2, 1, 3).contiguous()
-        heads_concat = heads_concat.view(heads_concat.size(0), -1, self.dimension)
-
-        return self.output_transform(heads_concat)
-
-
-class DividedAttentionSublayer(nn.Module):
-    def __init__(self, dimension, heads, max_pos_len, attention_dropout):
-        super(DividedAttentionSublayer, self).__init__()
-
-        self.dimension = dimension
-        self.heads = heads
-        self.max_pos_len = max_pos_len
-        self.scale = math.sqrt(dimension / heads)
-
-        self.pos_embedding = torch.randn(2*max_pos_len + 1, dimension // heads, device='cuda', requires_grad=True)
-        self.input_transform_q = nn.Linear(dimension, dimension, bias=False)
-        self.input_transform_k = nn.Linear(dimension, dimension, bias=False)
-        self.input_transform_v = nn.Linear(dimension, dimension, bias=False)
-        self.softmax = nn.Softmax(dim=3)
-        self.attention_dropout = nn.Dropout(attention_dropout)
-        self.output_transform = nn.Linear(dimension, dimension, bias=False)
-
-        #self.init_weights()
-
-    def init_weights(self):
-        nn.init.normal_(self.input_transform_q.weight, mean=0, std=np.sqrt(2.0 / (self.dimension + self.dimension // self.heads)))
-        nn.init.zeros_(self.input_transform_q.bias)
-        nn.init.normal_(self.input_transform_k.weight, mean=0, std=np.sqrt(2.0 / (self.dimension + self.dimension // self.heads)))
-        nn.init.zeros_(self.input_transform_k.bias)
-        nn.init.normal_(self.input_transform_v.weight, mean=0, std=np.sqrt(2.0 / (self.dimension + self.dimension // self.heads)))
-        nn.init.zeros_(self.input_transform_v.bias)
-        nn.init.xavier_normal_(self.output_transform.weight)
-        nn.init.zeros_(self.output_transform.bias)
-
-    def _split_heads(self, x):
-        x = x.view(x.size(0), -1, self.heads, self.dimension // self.heads)
+    def _split_heads(self, x: (B,T,D)) -> (B,H,T,D//H):
+        x: (B,T,H,D//H) = x.view(x.size(0), -1, self.heads, self.dimension // self.heads)
         return x.permute(0, 2, 1, 3).contiguous()
 
-    def forward(self, x_q, x_k, x_v, mask, sentence_lengths=None):
-        Q = self._split_heads(self.input_transform_q(x_q))
-        K = self._split_heads(self.input_transform_k(x_k))
-        V = self._split_heads(self.input_transform_v(x_v))
+    def forward(self, x_q: (B,T,D), x_k: (B,T,D), x_v: (B,T,D), mask: (B,1,1,T), sentence_lengths=None):
+        Q: (B,H,T_Q,D//H) = self._split_heads(self.input_transform_q(x_q))
+        K: (B,H,T_K,D//H) = self._split_heads(self.input_transform_k(x_k))
+        V: (B,H,T_K,D//H) = self._split_heads(self.input_transform_v(x_v))
 
         batch_size, _, len_q, _ = Q.size()
         len_k = K.size(2)
 
-        logits = torch.matmul(Q, K.transpose(2, 3))
+        logits: (B,H,T_Q,T_K) = torch.matmul(Q, K.transpose(2, 3))
 
         if sentence_lengths is None:
-            arranged_pos = arrange_char_pos_embedding(len_k, len_q, self.max_pos_len, self.pos_embedding)
-            Q_t = Q.permute(2, 0, 1, 3).view(len_q, batch_size * self.heads, -1)
-            pos_logits = torch.matmul(Q_t, arranged_pos.transpose(1, 2))
-            pos_logits = pos_logits.view(len_q, batch_size, self.heads, -1).permute(1, 2, 0, 3)
+            arranged_pos: (T_Q,T_K,D//H) = arrange_char_pos_embedding(len_k, len_q, self.max_pos_len, self.pos_embedding)
+            Q_t: (T_Q,B*H,D//H) = Q.permute(2, 0, 1, 3).view(len_q, batch_size * self.heads, -1)
+            pos_logits: (T_Q,B*H,T_K) = torch.matmul(Q_t, arranged_pos.transpose(1, 2))
+            pos_logits: (B,H,T_Q,T_K) = pos_logits.view(len_q, batch_size, self.heads, -1).permute(1, 2, 0, 3)
         else:
-            arranged_pos = arrange_word_pos_embedding(len_k, sentence_lengths, self.max_pos_len, self.pos_embedding)
-            pos_logits = torch.matmul(Q.transpose(0, 1), arranged_pos.transpose(1, 2))
-            pos_logits = pos_logits.transpose(0, 1)
+            arranged_pos: (B,T_K,D//H) = arrange_word_pos_embedding(len_k, sentence_lengths, self.max_pos_len, self.pos_embedding)
+            pos_logits: (H,B,T_Q,T_K) = torch.matmul(Q.transpose(0, 1), arranged_pos.transpose(1, 2))
+            pos_logits: (B,H,T_Q,T_K) = pos_logits.transpose(0, 1)
 
-        logits = (logits + pos_logits) / self.scale
+        logits: (B,H,T_Q,T_K) = (logits + pos_logits) / self.scale
         if mask is not None: logits.masked_fill_(mask, -np.inf)
-        indices = self.softmax(logits)
-        indices = self.attention_dropout(indices)
-        combined = torch.matmul(indices, V)
+        indices: (B,H,T_Q,T_K) = nn.functional.softmax(logits, dim=-1)
+        indices: (B,H,T_Q,T_K) = self.attention_dropout(indices)
+        combined: (B,H,T_Q,D//H) = torch.matmul(indices, V)
 
-        heads_concat = combined.permute(0, 2, 1, 3).contiguous()
-        heads_concat = heads_concat.view(heads_concat.size(0), -1, self.dimension)
+        heads_concat: (B,T_Q,H,D//H) = combined.permute(0, 2, 1, 3).contiguous()
+        heads_concat: (B,T_Q,D) = heads_concat.view(heads_concat.size(0), -1, self.dimension)
 
         return self.output_transform(heads_concat)
 
@@ -140,21 +100,13 @@ class DenseSublayer(nn.Module):
     def __init__(self, dimension, dropout):
         super(DenseSublayer, self).__init__()
 
-        self.linear_1 = nn.Linear(dimension, 4*dimension)
+        self.linear_1 = PositionLinear(dimension, 4*dimension)
         self.relu = nn.ReLU()
-        self.linear_2 = nn.Linear(4*dimension, dimension)
+        self.linear_2 = PositionLinear(4*dimension, dimension)
         self.dropout = nn.Dropout(dropout)
 
-        #self.init_weights()
-
-    def init_weights(self):
-        nn.init.xavier_normal_(self.linear_1.weight, gain=nn.init.calculate_gain('relu'))
-        nn.init.zeros_(self.linear_1.bias)
-        nn.init.xavier_normal_(self.linear_2.weight, gain=1)
-        nn.init.zeros_(self.linear_2.bias)
-
-    def forward(self, x):
-        x = self.relu(self.linear_1(x))
+    def forward(self, x: (B,T,D)) -> (B,T,D):
+        x: (B,T,4*D) = self.relu(self.linear_1(x))
         return self.dropout(self.linear_2(x))
 
 
@@ -162,15 +114,15 @@ class EncoderLayer(nn.Module):
     def __init__(self, dimension, heads, dropout, max_pos_len, attention_dropout):
         super(EncoderLayer, self).__init__()
 
-        self.attention = UniformAttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
+        self.attention = AttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
         self.dropout_1 = nn.Dropout(dropout)
         self.layer_norm_1 = nn.LayerNorm(dimension)
 
         self.dense_sublayer = DenseSublayer(dimension, dropout)
         self.layer_norm_2 = nn.LayerNorm(dimension)
 
-    def forward(self, x, mask):
-        attention = self.attention(x, mask)
+    def forward(self, x: (B,T,D), mask: (B,T,D,D)):
+        attention: (B,T,D) = self.attention(x, x, x, mask)
         x = self.layer_norm_1(x + self.dropout_1(attention))
 
         return self.layer_norm_2(x + self.dense_sublayer(x))
@@ -184,15 +136,15 @@ class DecoderLayer(nn.Module):
         self.heads = heads
         self.dropout = dropout
 
-        self.attention_1 = UniformAttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
+        self.attention_1 = AttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
         self.dropout_1 = nn.Dropout(dropout)
         self.layer_norm_1 = nn.LayerNorm(dimension)
 
-        self.attention_2 = DividedAttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
+        self.attention_2 = AttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
         self.dropout_2 = nn.Dropout(dropout)
         self.layer_norm_2 = nn.LayerNorm(dimension)
 
-        self.attention_3 = DividedAttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
+        self.attention_3 = AttentionSublayer(dimension, heads, max_pos_len, attention_dropout)
         self.dropout_3 = nn.Dropout(dropout)
         self.layer_norm_3 = nn.LayerNorm(dimension)
 
@@ -200,7 +152,7 @@ class DecoderLayer(nn.Module):
         self.layer_norm_4 = nn.LayerNorm(dimension)
 
     def forward(self, char_encoder_output, word_encoder_output, x, look_ahead_mask, char_padding_mask, word_padding_mask, sentence_lengths):
-        attention = self.attention_1(x, look_ahead_mask)
+        attention = self.attention_1(x, x, x, look_ahead_mask)
         x = self.layer_norm_1(x + self.dropout_1(attention))
 
         attention = self.attention_2(x, char_encoder_output, char_encoder_output, char_padding_mask)
@@ -290,30 +242,22 @@ class WordEmbedding(nn.Module):
         self.combined_linear = nn.Linear(args.dim + MorphoDataset.Dataset.EMBEDDING_SIZE, args.dim)
         self.combined_relu = nn.ReLU()
 
-        #self.init_weights()
-
-    def init_weights(self):
-        nn.init.kaiming_normal_(self.conv_linear.weight, nonlinearity='relu')
-        nn.init.zeros_(self.conv_linear.bias)
-        nn.init.kaiming_normal_(self.combined_linear.weight, nonlinearity='relu')
-        nn.init.zeros_(self.combined_linear.bias)
-
-    def forward(self, chars, word_embeddings, char_mask):
-        word_embeddings = self.word_dropout(word_embeddings)
+    def forward(self, chars: (S,W,C), word_embeddings: (S,W), char_mask):
+        word_embeddings: (S,W,D) = self.word_dropout(word_embeddings)
 
         sentences, words, char_len = chars.size()
-        char_embedding = self.embedding(chars)
-        char_embedding_flat = char_embedding.view(-1, char_len, self.dim).transpose(-2, -1)
+        char_embedding: (S,W,C,D) = self.embedding(chars)
+        char_embedding_flat: (S*W,D,C) = char_embedding.view(-1, char_len, self.dim).transpose(-2, -1)
 
         to_concat = []
         for convolution in self.convolutions:
             convoluted = convolution(char_embedding_flat)
             to_concat.append(convoluted.max(dim=-1).values)
         concated = torch.cat(to_concat, dim=-1)
-        convoluted_char_embedding = self.conv_relu(self.conv_linear(concated)).view(sentences, words, -1)
+        convoluted_char_embedding: (S,W,D) = self.conv_relu(self.conv_linear(concated)).view(sentences, words, -1)
 
-        full_embedding = self.combined_linear(torch.cat([word_embeddings, convoluted_char_embedding], dim=-1))
-        full_embedding = self.combined_relu(full_embedding)
+        full_embedding: (S,W,D) = self.combined_linear(torch.cat([word_embeddings, convoluted_char_embedding], dim=-1))
+        full_embedding: (S,W,D) = self.combined_relu(full_embedding)
 
         return chars[char_mask,:], char_embedding[char_mask,:,:], full_embedding
 
@@ -328,18 +272,12 @@ class Model(nn.Module):
         self._decoder = Decoder(num_target_chars, args.dim, args.heads, args.layers, args.dropout, args.duz, args.max_pos_len)
         self._tag_classifier = nn.Linear(args.dim, num_target_tags)
 
-        #self._init_weights()
-
-    def _init_weights(self):
-        nn.init.xavier_normal_(self._tag_classifier.weight)
-        nn.init.zeros_(self._tag_classifier.bias)
-
-    def _create_look_ahead_mask(self, target):
-        look_ahead = torch.ones(target.size(1), target.size(1), device='cuda', dtype=torch.uint8).triu(1)
-        padding = self._create_padding_mask(target)
+    def _create_look_ahead_mask(self, target: (B,T)) -> (B,1,T,T):
+        look_ahead: (T,T) = torch.ones(target.size(1), target.size(1), device='cuda', dtype=torch.uint8).triu_(1)
+        padding: (B,1,1,T) = self._create_padding_mask(target)
         return torch.max(padding, look_ahead)
 
-    def _create_padding_mask(self, seq):
+    def _create_padding_mask(self, seq: (B,T)) -> (B,1,1,T):
         seq = seq == 0
         seq.unsqueeze_(1).unsqueeze_(1)
         return seq
